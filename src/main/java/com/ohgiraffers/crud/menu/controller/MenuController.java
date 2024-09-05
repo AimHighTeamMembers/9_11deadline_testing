1ecc03d7e19adaf9cbc35ddd1ea9b4a3182e7650
scho123 <nvmsnach@gmail.com>
2024-09-05 16:55:40 +0900

    fix: directory 수정

--- src/main/java/com/ohgiraffers/crud/menu/controller/MenuController.java
+++ src/main/java/com/ohgiraffers/crud/menu/controller/MenuController.java
@@ -1,19 +1,12 @@
-package com.ohgiraffers.springboottest.menu.controller;
+package com.ohgiraffers.crud.menu.controller;
 
-import com.ohgiraffers.crud.menu.model.dto.CategoryDTO;
-import com.ohgiraffers.crud.menu.model.dto.MenuDTO;
 import com.ohgiraffers.crud.menu.model.service.MenuService;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.MessageSource;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.servlet.mvc.support.RedirectAttributes;
-
-import java.util.List;
-import java.util.Locale;
 
 @Controller
 @RequestMapping("/menu")
