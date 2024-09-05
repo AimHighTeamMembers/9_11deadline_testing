1ffd9986e3cfd4e8d350db3798f057f13593d852
kalien12 <kalien.lee.1102@gmail.com>
2024-09-05 17:49:18 +0900

    본인의 몇가지 기능을 구현하여 fetch - commit - merge 실행합니다.

--- src/main/java/com/ohgiraffers/crud/menu/model/service/MenuService.java
+++ src/main/java/com/ohgiraffers/crud/menu/model/service/MenuService.java
@@ -1,8 +1,12 @@
 package com.ohgiraffers.crud.menu.model.service;
 
 import com.ohgiraffers.crud.menu.model.dao.MenuMapper;
+import com.ohgiraffers.crud.menu.model.dto.CategoryDTO;
+import com.ohgiraffers.crud.menu.model.dto.MenuDTO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class MenuService {
@@ -14,5 +18,13 @@ public class MenuService {
         this.menuMapper = menuMapper;
     }
 
+    /* 설명. seojoon : findAllMenu() 기능 구현함*/
+    public List<MenuDTO> findAllMenu() {
+        return menuMapper.findAllMenu();
+    }
+
+    public List<CategoryDTO> findAllCategory() {
+        return menuMapper.findAllCategory();
+    }
 
 }
