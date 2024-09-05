97cde80357ec1a8de93f128a01748a2761b28885
scho123 <nvmsnach@gmail.com>
2024-09-05 16:30:51 +0900

    first commit

---
+++ src/main/java/com/ohgiraffers/springboottest/menu/controller/MenuController.java
@@ -0,0 +1,36 @@
+package com.ohgiraffers.springboottest.menu.controller;
+
+import com.ohgiraffers.crud.menu.model.dto.CategoryDTO;
+import com.ohgiraffers.crud.menu.model.dto.MenuDTO;
+import com.ohgiraffers.crud.menu.model.service.MenuService;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import java.util.List;
+import java.util.Locale;
+
+@Controller
+@RequestMapping("/menu")
+public class MenuController {
+
+    private static final Logger logger = LogManager.getLogger(MenuController.class);
+
+    private final MenuService menuService;
+    private final MessageSource messageSource;
+
+    @Autowired
+    public MenuController(MenuService menuService, MessageSource messageSource) {
+
+        this.menuService = menuService;
+        this.messageSource = messageSource;
+
+    }
+
+
+}
---
+++ src/main/java/com/ohgiraffers/springboottest/menu/model/service/MenuService.java
@@ -0,0 +1,23 @@
+package com.ohgiraffers.springboottest.menu.model.service;
+
+import com.ohgiraffers.crud.menu.model.dao.MenuMapper;
+import com.ohgiraffers.crud.menu.model.dto.CategoryDTO;
+import com.ohgiraffers.crud.menu.model.dto.MenuDTO;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+public class MenuService {
+
+    private final MenuMapper menuMapper;
+
+    @Autowired
+    public MenuService(MenuMapper menuMapper) {
+        this.menuMapper = menuMapper;
+    }
+
+
+}
